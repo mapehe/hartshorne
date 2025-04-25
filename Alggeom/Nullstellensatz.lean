e518163859c8ee2a4743653d9cffddda4f890eef
Matias Heikkilä <matias@three.consulting>
2025-04-25 12:16:43 +0300

    Define the Rabinowitsch homomorphism.

---
+++ Alggeom/Nullstellensatz.lean
@@ -0,0 +1,16 @@
+import Alggeom.Basic
+
+open Poly MvPolynomial
+
+-- Define the Rabinowitsch homomorphism.
+noncomputable def φ
+  {n : ℕ+} {k : Type*} [Field k] (g : k[n]) :
+  k [n + 1] → k(n) :=
+  fun f => f.eval₂
+    (algebraMap k (k(n)))
+    (λ (i : Fin (n+1)) =>
+      if h : (i : ℕ) < n then
+        algebraMap (k[n]) (k (n)) (X ⟨i, h⟩)
+      else
+        (algebraMap (k[n]) (k (n)) g)⁻¹)
+
